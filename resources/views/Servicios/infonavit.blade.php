34d9592399459ee76853fcb0dde4b1fa563f0d6f
emarketingcontacto <emarketing.contacto@gmail.com>
2024-02-09 00:35:51 -0600

    infonavit image contactcontroller

--- resources/views/Servicios/infonavit.blade.php
+++ resources/views/Servicios/infonavit.blade.php
@@ -2,48 +2,53 @@
 @section('main-content')
 
 <div class="container pb-5">
-    <h1>Infonavit</h1>
-    <span>
         <h3>Asesoramiento en trámites que el Infonavit pone a tu disposición, detalle de requisitos y formatos que debes presentar.</h3>
-    </span>
-    <div class="d-flex w-100 gap-3" >
-        <div class="col">
-        <span>
-            <ul>
-                <li>Acceso a Información</li>
-                <li>Aclaración de Pagos</li>
-                <li>Asociación y Disociación de NRP</li>
-                <li>Cancelación de Hipoteca</li>
-                <li>Carta de Saldo para Pasivo Infonavit</li>
-                <li>Consulta de Estado de Cuenta Empresarial </li>
-                <li>Convenio Tradicional en Parcialidades Sin Beneficios</li>
-                <li>Correción de Datos</li>
-                <li>Devolución de Pagos de Crédito No Originado</li>
-                <li>Devolución de Pagos en Exceso, Duplicados e Indebidos</li>
-                <li>Devolución de Fondo de Ahorro</li>
-                <li>Dictámenes de Infonavit</li>
-                <li>Ejercicio de Derechos ARCO</li>
-                <li>Esquema Flexible de Desempleo</li>
-                <li>Inscripción y Formalización de Crédito</li>
-                <li>Liberación de Creditos</li>
-                <li>Liquidación Por Autoseguro</li>
-                <li>Medios de Pago</li>
-                <li>Separación de Cuentas</li>
-                <li>Solicitud o Aclaración de Prórroga</li>
-                <li>Solicitud o Aclaración de Créditos</li>
-                <li>Solicitud y Seguimiento de Devolución del Saldo a Subcuenta de Vivienda</li>
-                <li>Solicitud o Candelación de Crédito</li>
-                <li>Unificación de Cuentas</li>
-            </ul>
-        </span>
+    <div class="d-grid w-100 gap-3" >
+        <div class="row">
+            <div class="col col-span-6">
+                <center>
+                    <img src="{{asset('Images/nfonavit.jpg')}}" alt="infonavit" width="80%" style="border-radius: 10%">
+                </center>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col mx-2">
+                <ul>
+                    <li>Acceso a Información</li>
+                    <li>Aclaración de Pagos</li>
+                    <li>Asociación y Disociación de NRP</li>
+                    <li>Cancelación de Hipoteca</li>
+                    <li>Carta de Saldo para Pasivo Infonavit</li>
+                    <li>Consulta de Estado de Cuenta Empresarial </li>
+                    <li>Convenio Tradicional en Parcialidades Sin Beneficios</li>
+                    <li>Correción de Datos</li>
+                    <li>Devolución de Pagos de Crédito No Originado</li>
+                    <li>Devolución de Pagos en Exceso, Duplicados e Indebidos</li>
+                    <li>Devolución de Fondo de Ahorro</li>
+                    <li>Dictámenes de Infonavit</li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul>
+                    <li>Ejercicio de Derechos ARCO</li>
+                    <li>Esquema Flexible de Desempleo</li>
+                    <li>Inscripción y Formalización de Crédito</li>
+                    <li>Liberación de Creditos</li>
+                    <li>Liquidación Por Autoseguro</li>
+                    <li>Medios de Pago</li>
+                    <li>Separación de Cuentas</li>
+                    <li>Solicitud o Aclaración de Prórroga</li>
+                    <li>Solicitud o Aclaración de Créditos</li>
+                    <li>Solicitud y Seguimiento de Devolución del Saldo a Subcuenta de Vivienda</li>
+                    <li>Solicitud o Candelación de Crédito</li>
+                    <li>Unificación de Cuentas</li>
+                </ul>
+            </div>
+        </div>
+
 
     </div>
-    <div class="col">
-        <img src="{{asset('Images/nfonavit.jpg')}}" alt="infonavit" width="80%" style="border-radius: 10%">
-    </div>
-
-    </div>
-
 </div>
 
 @endsection
